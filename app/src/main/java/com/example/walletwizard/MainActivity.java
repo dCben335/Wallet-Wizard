e919a90e7fe746d697ff2cb169cb809f7cfb4c4a
dCben335 <benoit.cabocel.335@gmail.com>
2024-02-11 14:55:37 +0100

    start the refocturing of the money converting app projetct

---
+++ app/src/main/java/com/example/walletwizard/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.walletwizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setFragmentListener();
+
+        // Load the initial fragment
+        loadFragment(new HomeFragment());
+    }
+
+    protected void setFragmentListener() {
+        swichToFragmentListener(findViewById(R.id.map_button), new MapFragment());
+        swichToFragmentListener(findViewById(R.id.home_button), new HomeFragment());
+        swichToFragmentListener(findViewById(R.id.convert_button), new HomeFragment());
+
+    }
+
+    // Method to load fragments
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    protected void swichToFragmentListener(Button button, Fragment fragment) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(fragment);
+            }
+        });
+    }
+}
