ecaa4c362800745816c9979ccd4492410f5f6962
dCben335 <benoit.cabocel.335@gmail.com>
2024-02-13 02:43:42 +0100

    Create stable version of the map

--- app/src/main/java/com/example/walletwizard/Fragment/MapFragment.java
+++ app/src/main/java/com/example/walletwizard/Fragment/MapFragment.java
@@ -1,32 +1,23 @@
 package com.example.walletwizard.Fragment;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import com.example.walletwizard.Utils.LocationHandler;
 import com.mapbox.mapboxsdk.annotations.Icon;
 import com.mapbox.mapboxsdk.annotations.IconFactory;
-
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.example.walletwizard.R;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.WellKnownTileServer;
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -34,31 +25,26 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import java.text.DecimalFormat;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment  {
     private View rootView;
+    private String mapStyleUrl;
     private Context context;
-
-
     private MapView mapView;
     private MapboxMap map;
-    private String mapStyleUrl;
+    private double currentLatitude;
+    private double currentLongitude;
 
-    private static final int REQUEST_LOCATION_PERMISSION = 1001;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-
-    public double currentLatitude;
-    public double currentLongitude;
-
-
-
+    public LocationHandler locationHandler;
+    private Marker ownMarker;
+    private Marker openedMarker;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         context = requireContext();
-
         mapStyleUrl = initiateMapSettings();
         rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
         return rootView;
     }
 
@@ -67,33 +53,12 @@ public class MapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mapView = rootView.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = setLocationListener();
-
-
-        if (!isLocationEnabled(context)) {
-            showLocationSettingsDialog(context);
-        }
-
-
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                (Activity) context,
-                new String[] {
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                },
-                REQUEST_LOCATION_PERMISSION
-            );
-        } else {
-            startLocationUpdates();
-        }
-
 
         injectMap(mapStyleUrl);
+
+        locationHandler = new LocationHandler(context, this);
+        locationHandler.startLocationUpdates();
     }
-
-
-
 
     protected String initiateMapSettings() {
         String apiKey = "uBctdXB4pPaJOqBcg0O8";
@@ -109,24 +74,45 @@ public class MapFragment extends Fragment {
     protected void injectMap(String styleUrl) {
         mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(new Style.Builder().fromUrl(styleUrl), style -> {
             map = mapboxMap;
-            if (currentLatitude != 0.0 && currentLongitude != 0.0) {
-                Icon currentPositionIcon = getMarkerIcon(R.drawable.marker);
-                Log.d("current lat", String.valueOf(currentLatitude));
-                Log.d("current long", String.valueOf(currentLongitude));
-
-                createMarker(currentPositionIcon, currentLatitude, currentLongitude, "Votre Position", "");
-            }
-
-            mapboxMap.setCameraPosition(new CameraPosition.Builder()
-                .target(new LatLng(47, 5))
-                .zoom(10)
-                .build()
-            );
+            if (isLocation()) replaceOwnPositionMarker();
+            setCamera();
         }));
     }
 
-    private void createMarker(Icon customMarkerIcon, double latitude, double longitude, String title, String description) {
-        addMarker(
+    private void replaceOwnPositionMarker() {
+        if (ownMarker != null) {
+            map.removeMarker(ownMarker);
+        }
+
+        Icon currentPositionIcon = getMarkerIcon(R.drawable.marker);
+        ownMarker = createMarker(currentPositionIcon, currentLatitude, currentLongitude, "Votre Position", "");
+    }
+
+
+    public void updateLocation(@NonNull Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        if (map != null) {
+            replaceOwnPositionMarker();
+            setCamera();
+        }
+    }
+
+    private void setCamera() {
+        double initialCameraLat = isLocation() ? currentLatitude : 1;
+        double initialCameraLong = isLocation() ? currentLongitude : 1;
+        int initalZoom =  isLocation() ? 10 : 1;
+
+        map.setCameraPosition(new CameraPosition.Builder()
+                .target(new LatLng(initialCameraLat, initialCameraLong))
+                .zoom(initalZoom)
+                .build()
+        );
+    }
+
+    private Marker createMarker(Icon customMarkerIcon, double latitude, double longitude, String title, String description) {
+        return addMarker(
             new LatLng(latitude, longitude),
             title,
             description,
@@ -134,89 +120,22 @@ public class MapFragment extends Fragment {
         );
     }
 
-
-
-
-
-
-    public static void showLocationSettingsDialog(final Context context) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-        builder.setMessage("La localisation est recommandée pour utiliser cette partie de l'application");
-        builder.setPositiveButton("Activer", (dialog, which) -> {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            context.startActivity(intent);
-        });
-
-        builder.setNegativeButton("Continuer", (dialog, which) -> dialog.dismiss());
-        builder.setCancelable(true);
-        builder.show();
-    }
-
-    public static boolean isLocationEnabled(Context context) {
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        return locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-    }
-
-
-
-    protected LocationListener setLocationListener() {
-        return new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                currentLatitude = location.getLatitude();
-                currentLongitude = location.getLongitude();
-                Log.d("current lat", String.valueOf(currentLatitude));
-                Log.d("current long", "dqzddqzdkzqdqbhdqd");
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            @Override
-            public void onProviderEnabled(@NonNull String provider) {}
-
-            @Override
-            public void onProviderDisabled(@NonNull String provider) {}
-        };
-    }
-
-    private void startLocationUpdates() {
-        try {
-
-            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (lastKnownLocation != null) {
-                locationListener.onLocationChanged(lastKnownLocation);
-            }
-
-
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                0,
-                0,
-                locationListener
-            );
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-    private void addMarker(LatLng latLng, String title, String description, Icon icon) {
+    private Marker addMarker(LatLng latLng, String title, String description, Icon icon) {
         MarkerOptions markerOptions = new MarkerOptions()
                 .position(latLng)
                 .title(title)
                 .snippet(description)
                 .icon(icon);
 
-        map.addMarker(markerOptions);
+        Marker newMarker = map.addMarker(markerOptions);
 
         map.setOnMarkerClickListener(marker -> {
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15.0));
+            marker.showInfoWindow(map, mapView);
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 12.0));
             return true;
         });
+
+        return newMarker;
     }
     private Icon getMarkerIcon(int src) {
         Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), src);
@@ -225,9 +144,15 @@ public class MapFragment extends Fragment {
         int height = 50;
         Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, width, height, false);
 
-        Icon customMarkerIcon = IconFactory.getInstance(context).fromBitmap(resizedBitmap);
-        return customMarkerIcon;
+        return IconFactory.getInstance(context).fromBitmap(resizedBitmap);
     }
+
+
+
+    public boolean isLocation() {
+        return currentLatitude != 0.0 && currentLongitude != 0.0;
+    }
+
     @Override
     public void onStart() {
         super.onStart();
--- app/src/main/java/com/example/walletwizard/Utils/LocationHandler.java
+++ app/src/main/java/com/example/walletwizard/Utils/LocationHandler.java
@@ -1,41 +1,130 @@
 package com.example.walletwizard.Utils;
 
 
-// LocationManager.java
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.example.walletwizard.Fragment.MapFragment;
+
 public class LocationHandler {
+
+    private final Context context;
+    private final MapFragment mapFragment;
+    private final LocationManager locationManager;
 
     private static final int REQUEST_LOCATION_PERMISSION = 1001;
 
-    private final Context context;
-    private final LocationListener locationListener;
 
-    public LocationHandler(Context context, LocationListener locationListener) {
+    public LocationHandler(Context context, MapFragment mapFragment) {
         this.context = context;
-        this.locationListener = locationListener;
+        this.mapFragment = mapFragment;
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
     }
 
+    @SuppressLint("MissingPermission")
     public void startLocationUpdates() {
-        android.location.LocationManager locationManager = (android.location.LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null) {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
-            } else {
-                locationManager.requestLocationUpdates(android.location.LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                Location lastKnownLocation = locationManager.getLastKnownLocation(android.location.LocationManager.GPS_PROVIDER);
-                if (lastKnownLocation != null) {
-                    locationListener.onLocationChanged(lastKnownLocation);
-                }
-            }
+        if (mapFragment.isLocation()) return;
+
+        if (!isLocationPermissionGranted()) {
+            requestLocationPermission();
+            return;
         }
+
+        if (!isLocationEnabled()) {
+            if (!getLastKnowLocation()) {
+                displayLocationDialog();
+            }
+            return;
+        }
+
+        getLastKnowLocation();
+        requestLocationUpdates();
     }
+
+
+
+    private void displayLocationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setMessage("La localisation est recommandée pour utiliser cette partie de l'application");
+        builder.setPositiveButton("Activer", (dialog, which) -> {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            context.startActivity(intent);
+            dialog.dismiss();
+        });
+
+        builder.setNegativeButton("Continuer", (dialog, which) -> dialog.dismiss());
+        builder.setCancelable(true);
+        builder.show();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocationUpdates() {
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0,
+            1000,
+            locationListener
+        );
+    }
+
+    @SuppressLint("MissingPermission")
+    private boolean getLastKnowLocation() {
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (lastKnownLocation != null) {
+            locationListener.onLocationChanged(lastKnownLocation);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            (Activity) context,
+            new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION
+            },
+            REQUEST_LOCATION_PERMISSION
+        );
+    }
+    private boolean isLocationEnabled() {
+        return locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private boolean isLocationPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            mapFragment.updateLocation(location);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            if (mapFragment.isLocation()) return;
+            getLastKnowLocation();
+            requestLocationUpdates();
+        }
+    };
 }
