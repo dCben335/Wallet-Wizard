51399dd77796b1f4e063435f552540e327157a44
chen <chenraptor@outlook.fr>
2024-02-16 21:17:05 +0100

    fragment_convert_base

--- app/src/main/java/com/example/walletwizard/Fragments/ConvertFragment.java
+++ app/src/main/java/com/example/walletwizard/Fragments/ConvertFragment.java
@@ -22,4 +22,6 @@ public class ConvertFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_convert, container, false);
     }
+
+
 }
