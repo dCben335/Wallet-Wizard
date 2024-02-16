12c8ce270b8d832d3c80519c575faeb4230f6ecb
chen <chenraptor@outlook.fr>
2024-02-16 15:23:47 +0100

    ConvertFragment updated

--- app/src/main/java/com/example/walletwizard/Fragment/ConvertFragment.java
+++ app/src/main/java/com/example/walletwizard/Fragment/ConvertFragment.java
@@ -20,6 +20,6 @@ public class ConvertFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return inflater.inflate(R.layout.fragment_convert, container, false);
     }
 }
