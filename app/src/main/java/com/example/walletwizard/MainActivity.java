947f6b9496372f2b0e2e38e287ec759a7cefd7fd
dCben335 <benoit.cabocel.335@gmail.com>
2024-02-20 01:48:52 +0100

    Made the app fullscreen

--- app/src/main/java/com/example/walletwizard/MainActivity.java
+++ app/src/main/java/com/example/walletwizard/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import com.example.walletwizard.Fragments.ConverterFragment;
@@ -32,13 +36,34 @@ public class MainActivity extends AppCompatActivity {
     public final int toLeftAnimation = R.anim.fragment_enter_animation;
     public final int toRightAnimation = R.anim.fragment_exit_animation;
 
+    private int currentApiVersion;
+
+    final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        getFullScreenApp();
+
         setContentView(R.layout.activity_main);
         loadFragment(homeFragment, 0);
+
         setFields();
         setFragmentButtons();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) getFullScreenApp();
     }
 
     public void changeFragment(RelativeLayout container, ImageButton button, Fragment fragment, int enterAnimation) {
@@ -104,5 +129,12 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+
+    private void getFullScreenApp() {
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+        }
     }
 }
